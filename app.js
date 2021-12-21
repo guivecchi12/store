cf70af7c16045bc7dc527081a4933380307b2036
Guilherme-Vecchi <guivecchi12@gmail.com>
2021-12-20 18:02:34 -0700

    helmet

--- app.js
+++ app.js
@@ -13,6 +13,28 @@ app.use(cors())
 app.use(helmet())
 app.use(cookieParser())
 
+app.use(
+    // [
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        connectSrc: ["'self'", 'https://checkout.stripe.com'],
+        frameSrc: ["'self'", 'https://checkout.stripe.com'],
+        childSrc: ["'self'", 'https://checkout.stripe.com'],
+        scriptSrc: ["'self'", 'https://checkout.stripe.com'],
+        styleSrc: [
+          "'self'",
+          'https://fonts.googleapis.com',
+          'https://checkout.stripe.com',
+        ],
+        fontSrc: ["'self'", 'https://fonts.gstatic.com'],
+        imgSrc: ["'self'", 'https://*.stripe.com', 'https://res.cloudinary.com'],
+        baseUri: ["'self'"],
+      },
+    })
+    // ]
+  )
+
 app.use(express.static(path.join(__dirname, 'build')))
 app.use(express.json())
 
